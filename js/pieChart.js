81808677dd408ea8326d4f8a97a420a30a45bb38
Danilo da Silva <danilodasilva@iMac-Pro-de-Danilo.local>
2020-11-24 00:49:34 -0300

    refactoring pieChart

--- js/pieChart.js
+++ js/pieChart.js
@@ -1,6 +1,4 @@
-
 var ctx = document.getElementById("pie-chart");
-
 var data = {
     datasets: [{
         data: [10, 20, 30],
@@ -10,8 +8,6 @@ var data = {
           '#01BDD4',
       ],
     }],
-
-    // These labels appear in the legend and in the tooltips when hovering different arcs
     labels: [
         'Student',
         'Teacher',
@@ -23,18 +19,13 @@ var myPieChart = new Chart(ctx,{
     data: data,
     options: {
       layout: {
-        // padding: 30,
-        // position: 'absolute',
       },
       legend: {    
         position : 'right',
-        // reverse: false,    
       },  
       responsive: true,
       maintainAspectRatio: false,
-      // aspectRatio: 2,
     }
-    
 });
 
 
